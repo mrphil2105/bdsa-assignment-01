ed73494efcc2f26979098a77ded626f41969b414
Philip Mørch <mrphil2105@gmail.com>
2022-09-09 13:41:48 +0200

    Provide test for Flatten

--- Assignment1.Tests/IteratorsTests.cs
+++ Assignment1.Tests/IteratorsTests.cs
@@ -1,5 +1,20 @@
+using Xunit;
+
 namespace Assignment1.Tests;
 
 public class IteratorsTests
 {
+    [Fact]
+    public void Flatten_FlattensEnumerable_WhenGivenNumbers()
+    {
+        // Arrange
+        var unflattened = new[] { new[] { 1, 2, 3 }, new[] { 4, 5 }, new[] { 6, 7, 8, 9 } };
+        var flattened = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+        // Act
+        var result = Iterators.Flatten(unflattened);
+
+        // Assert
+        Assert.Equal(flattened, result);
+    }
 }
