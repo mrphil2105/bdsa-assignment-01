5bbda3c8bca282c2122da2cc651dd5bf190fca55
Philip Mørch <mrphil2105@gmail.com>
2022-09-10 14:21:13 +0200

    Implement SplitLine with yield

--- Assignment1/RegExpr.cs
+++ Assignment1/RegExpr.cs
@@ -1,8 +1,21 @@
+using System.Text.RegularExpressions;
+
 namespace Assignment1;
 
 public static class RegExpr
 {
-    public static IEnumerable<string> SplitLine(IEnumerable<string> lines) => throw new NotImplementedException();
+    public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
+    {
+        foreach (var line in lines)
+        {
+            var matches = Regex.Matches(line, @"\w+");
+
+            foreach (Match match in matches)
+            {
+                yield return match.Value;
+            }
+        }
+    }
 
     public static IEnumerable<(int width, int height)> Resolution(string resolutions) => throw new NotImplementedException();
 
