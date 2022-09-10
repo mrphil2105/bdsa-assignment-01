6c456b2f6eea47edeee05b3cdc2b4d084229ca3e
Philip Mørch <mrphil2105@gmail.com>
2022-09-10 14:10:10 +0200

    Provide test for SplitLine

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -1,5 +1,35 @@
+using Xunit;
+
 namespace Assignment1.Tests;
 
 public class RegExprTests
 {
+    public static IEnumerable<object[]> SentencesAndWords => new List<object[]>
+    {
+        new object[]
+        {
+            new[]
+            {
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                "Nam porta nisl nisl, in elementum est convallis iaculis."
+            },
+            new[]
+            {
+                "Lorem", "ipsum", "dolor", "sit", "amet", "consectetur", "adipiscing", "elit", "Nam",
+                "porta", "nisl", "nisl", "in", "elementum", "est", "convallis", "iaculis"
+            }
+        },
+    };
+
+    [Theory]
+    [MemberData(nameof(SentencesAndWords))]
+    public void SplitLine_SplitsSentencesToWords_WhenGivenSentences(IEnumerable<string> sentences,
+        IEnumerable<string> words)
+    {
+        // Act
+        var result = RegExpr.SplitLine(sentences);
+
+        // Assert
+        Assert.Equal(words, result);
+    }
 }
