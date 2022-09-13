af863b1fb4fcb0a6d04d24d81fe659934b6e6b3c
Philip Mørch <mrphil2105@gmail.com>
2022-09-13 13:34:23 +0200

    Add tests for nested HTML tags

--- Assignment1.Tests/RegExprTests.cs
+++ Assignment1.Tests/RegExprTests.cs
@@ -26,6 +26,29 @@ public class RegExprTests
         }
     };
 
+    public static IEnumerable<object[]> ExerciseHtmlAndTags => new List<object[]>
+    {
+        new object[]
+        {
+            "<div>\n    <p>A <b>regular expression</b>, <b>regex</b> or <b>regexp</b> (sometimes called a <b>rational expression</b>) is, in <a href=\"https://en.wikipedia.org/wiki/Theoretical_computer_science\" title=\"Theoretical computer science\">theoretical computer science</a> and <a href=\"https://en.wikipedia.org/wiki/Formal_language\" title=\"Formal language\">formal language</a> theory, a sequence of <a href=\"https://en.wikipedia.org/wiki/Character_(computing)\" title=\"Character (computing)\">characters</a> that define a <i>search <a href=\"https://en.wikipedia.org/wiki/Pattern_matching\" title=\"Pattern matching\">pattern</a></i>. Usually this pattern is then used by <a href=\"https://en.wikipedia.org/wiki/String_searching_algorithm\" title=\"String searching algorithm\">string searching algorithms</a> for \"find\" or \"find and replace\" operations on <a href=\"https://en.wikipedia.org/wiki/String_(computer_science)\" title=\"String (computer science)\">strings</a>.</p>\n</div>\n",
+            "a",
+            new[]
+            {
+                "theoretical computer science", "formal language", "characters", "pattern",
+                "string searching algorithms", "strings"
+            }
+        },
+        new object[]
+        {
+            "<div>\n    <p>The phrase <i>regular expressions</i> (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing <u>patterns</u> that matching <em>text</em> need to conform to.</p>\n</div>\n",
+            "p",
+            new[]
+            {
+                "The phrase regular expressions (and consequently, regexes) is often used to mean the specific, standard textual syntax for representing patterns that matching text need to conform to."
+            }
+        }
+    };
+
     [Theory]
     [MemberData(nameof(SentencesAndWords))]
     public void SplitLine_SplitsSentencesToWords_WhenGivenSentences(IEnumerable<string> sentences,
@@ -55,6 +78,7 @@ public class RegExprTests
     [InlineData("<a href=\"https://www.google.com/search?q=foo\">foo</a>", "a", new[] { "foo" })]
     [InlineData("<span>foo</span><h1 id=\"section-0\">Title</h1><span class=\"fancy-text\">bar</span>", "span", new[] { "foo", "bar" })]
     [InlineData("<p></p>", "p", new[] { "" })]
+    [MemberData(nameof(ExerciseHtmlAndTags))]
     public void InnerText_ReturnsInnerTexts_WhenGivenHtmlAndTag(string html, string tag, string[] innerTexts)
     {
         // Act
